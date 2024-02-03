a2dab1512b6708354177f2524816a403e1951525
hms5232 <hms5232@hhming.moe>
2024-02-03 21:46:37 +0800

    feat: run and received a message include ip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::env;
+use dotenvy::dotenv;
+use frankenstein::{Api, SendMessageParams, TelegramApi};
 use local_ip_address::local_ip;
 
 fn main() {
-    println!("Hello, {}", get_ip());
+    // If .env file exists, load into environment variable.
+    dotenv().ok();
+    // get config
+    let token_binding = env::var("TOKEN").expect("TOKEN not set");
+    let token = token_binding.as_str();
+    let notify_user_id: i64 = env::var("NOTIFY_USER_ID").expect("NOTIFY_USER_ID not set").parse::<i64>().expect("NOTIFY_USER_ID type is wrong.");
+
+    // make a message
+    let message = SendMessageParams::builder()
+        .chat_id(notify_user_id)
+        .text(get_ip())
+        .build();
+    // send
+    Api::new(token).send_message(&message).unwrap();
 }
 
 /// Get local IP address
