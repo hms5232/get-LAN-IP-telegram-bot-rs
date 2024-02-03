c3dc8f954b19a8df819a0447382b4b35ed12ed5f
hms5232 <hms5232@hhming.moe>
2024-02-03 15:49:36 +0800

    feat: get local ip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use local_ip_address::local_ip;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, {}", get_ip());
+}
+
+/// Get local IP address
+fn get_ip() -> String {
+    local_ip().unwrap().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_ip_v4() {
+        assert!(get_ip().contains("."));
+        assert_eq!(4, get_ip().split(".").collect::<Vec<_>>().len());
+    }
 }
