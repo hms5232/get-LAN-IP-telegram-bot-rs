33e33b6846d404fd61715b369438fa4d5df6bbe0
hms5232 <hms5232@hhming.moe>
2024-02-24 21:53:43 +0800

    refactor: make env static for sharing

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,36 @@ use dotenvy::dotenv;
 use frankenstein::{Api, SendMessageParams, TelegramApi};
 use local_ip_address::local_ip;
 use std::env;
+use lazy_static::lazy_static;
 
-fn main() {
-    // If .env file exists, load into environment variable.
-    dotenv().ok();
-    // get config
-    let token_binding = env::var("TOKEN").expect("TOKEN not set");
-    let token = token_binding.as_str();
-    let notify_user_id: i64 = env::var("NOTIFY_USER_ID")
+lazy_static! {
+    static ref NOTIFY_USER_ID: i64 = {
+        // If .env file exists, load into environment variable.
+        dotenv().ok();
+
+        env::var("NOTIFY_USER_ID")
         .expect("NOTIFY_USER_ID not set")
         .parse::<i64>()
-        .expect("NOTIFY_USER_ID type is wrong.");
+        .expect("NOTIFY_USER_ID type is wrong.")
+    };
 
+    static ref TOKEN: &'static str = {
+        // If .env file exists, load into environment variable.
+        dotenv().ok();
+
+        let token_binding: String = env::var("TOKEN").expect("TOKEN not set");
+        Box::leak(token_binding.into_boxed_str())
+    };
+}
+
+fn main() {
     // make a message
     let message = SendMessageParams::builder()
-        .chat_id(notify_user_id)
+        .chat_id(*NOTIFY_USER_ID)
         .text(get_ip())
         .build();
     // send
-    Api::new(token).send_message(&message).unwrap();
+    Api::new(*TOKEN).send_message(&message).unwrap();
 }
 
 /// Get local IP address
